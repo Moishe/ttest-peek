ae13cb02e6bc2bc85e1a40e3277c8fe7aa5013eb
Moishe Lettvin <moishel@gmail.com>
2016-02-17 18:22:46 -0700

    Break VectorSample into its own file

--- ttest.js
+++ ttest.js
@@ -7,38 +7,6 @@ function normalRand() {
     return [r1, r2];
 }
 
-function VectorSample() {
-  this.n = 0;
-  this.mean = 0;
-  this.m2 = 0;
-  this.buckets = [];
-}
-
-VectorSample.prototype.update = function(x) {
-  this.n += 1;
-  var delta = (x - this.mean);
-  this.mean += delta / this.n;
-  this.m2 += delta*(x - this.mean);
-
-  var bucket = Math.floor((x + 2) * 5);
-  if (bucket < 0 || bucket > 20) {
-    return;
-  }
-  if (!this.buckets[bucket]) {
-    this.buckets[bucket] = 1;
-  } else {
-    this.buckets[bucket] += 1;
-  }
-};
-
-VectorSample.prototype.stdev = function() {
-  return Math.sqrt(this.m2);
-}
-
-VectorSample.prototype.dump = function() {
-  console.log([this.mean, this.m2, this.buckets].join());
-};
-
 var PiD2 = Math.PI / 2;
 
 // note that I copied a bunch of this code from:
