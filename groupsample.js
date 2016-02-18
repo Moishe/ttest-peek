d750ac2d973af0e42bb1045aa56c641e4d588944
Moishe Lettvin <moishel@gmail.com>
2016-02-17 18:36:57 -0700

    Add chi-squared test to GroupSample

--- groupsample.js
+++ groupsample.js
@@ -2,15 +2,49 @@ function GroupSample() {
   this.groups = {};
 }
 
-GroupSample.prototype.update = function(group) {
+GroupSample.prototype.update = function(group, value) {
+  if (!value) {
+    value = 1;
+  }
+
   if (!this.groups[group]) {
-    this.groups[group] = 1;
+    this.groups[group] = value;
   } else {
-    this.groups[group] += 1;
+    this.groups[group] += value;
   }
 };
+
+GroupSample.prototype.df = function() {
+  return Object.keys(this.groups).length;
+}
 
 GroupSample.prototype.dump = function() {
 
 }
 
+GroupSample.prototype.chiSquare = function(expected) {
+  var cs = 0;
+  for (var group in expected.groups) {
+    if (expected.groups.hasOwnProperty(group)) {
+      var o;
+      if (!this.groups[group]) {
+        o = 0;
+      } else {
+        o = this.groups[group];
+      }
+      e = expected.groups[group];
+      cs += Math.pow(o - e, 2) / e;
+    }
+  }
+  return cs;
+}
+
+var expected = new GroupSample();
+expected.update('red', 100);
+expected.update('blue', 50);
+
+var observed = new GroupSample();
+observed.update('red', 90);
+observed.update('blue', 60);
+
+console.log('chi-square: ' + observed.chiSquare(expected));
--- ttest.js
+++ ttest.js
@@ -7,46 +7,6 @@ function normalRand() {
     return [r1, r2];
 }
 
-var PiD2 = Math.PI / 2;
-
-// note that I copied a bunch of this code from:
-// https://home.ubalt.edu/ntsbarsh/business-stat/otherapplets/pvalues.htm#rtdist
-function StatCom(q, i, j, b) {
-  var zz = 1;
-  var z = zz;
-  var k = i;
-  while(k <= j) {
-    zz = zz * q * k / (k - b);
-    z = z + zz;
-    k = k + 2
-  }
-  return z
-}
-
-function ttest(a, b) {
-  astdev = a.stdev();
-  bstdev = b.stdev();
-  tvalue = (a.mean - b.mean) / ((astdev / a.n) + (bstdev / b.n));
-
-  astdev2 = astdev * astdev;
-  bstdev2 = bstdev * bstdev;
-  df = ((astdev2 / a.n) + (bstdev2 / b.n)) / ((1 / a.n) * (astdev2 / a.n) + (1 / b.n) * (bstdev2 / b.n));
-
-  var w = Math.abs(tvalue) / Math.sqrt(df);
-  var th = Math.atan(w)
-
-  var sth = Math.sin(th);
-  var cth = Math.cos(th)
-  if ((df % 2) == 1) {
-    p = 1 - (th + sth * cth * StatCom(cth * cth, 2, df-3, -1)) / PiD2;
-  }
-  else {
-    p = 1 - sth * StatCom(cth * cth, 1, df-3, -1);
-  }
-
-  return p;
-}
-
 function runTestWithVectors() {
   var numPairs = 10000;
   var samplePairs = [];
@@ -100,4 +60,4 @@ function runTestWithVectors() {
   console.log(triggered / numPairs + " false positive percentage.");
 }
 
-runTestWithVectors();
+//runTestWithVectors();
